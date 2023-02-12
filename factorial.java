9260850aecea7e2f35520cc7e7aec1e4cf5a45bd
marioperales10 <123986248+marioperales10@users.noreply.github.com>
2023-02-12 14:46:26 +0100

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		Scanner scanInput = new Scanner(System.in);
+		System.out.print("Introduzca un número: ");
+		long num = scanInput.nextLong();
+		long fac = 1;
+		for(long i=1; i<=num; ++i) {
+			fac = fac * i;
+		}
+		System.out.println("El factorial de "+num+" es"+fac);
+		scanInput.close();
+
+	}
+
+}
